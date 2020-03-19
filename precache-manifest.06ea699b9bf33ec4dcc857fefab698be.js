a40568ef5dccdf1a1b3053cf1214ac5552f67f0d
salvosaieva <62262507+salvosaieva@users.noreply.github.com>
2020-03-19 12:11:10 +0100

    Updates

---
+++ precache-manifest.06ea699b9bf33ec4dcc857fefab698be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6bbed381e48aacf398ab728e5f114d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ca9596c51e3de2b98dc1",
+    "url": "./static/css/main.3cf93cf6.chunk.css"
+  },
+  {
+    "revision": "f9362639042603f9681a",
+    "url": "./static/js/2.2c78229e.chunk.js"
+  },
+  {
+    "revision": "ca9596c51e3de2b98dc1",
+    "url": "./static/js/main.2432de7e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "9d0bade06e0a92872a10fbe61c1305d1",
+    "url": "./static/media/covid.9d0bade0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "./static/media/shape.208f3d38.svg"
+  }
+]);
